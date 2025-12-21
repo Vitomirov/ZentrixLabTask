ed80c772e27668570e55ffcc1ec7c87a62665446
Dejan Vitomirov <dejan.vitomirov@gmail.com>
2025-12-21 13:03:11 +0100

    feat: add JWT authentication middleware and protected test endpoint

--- services/character-service/src/main.ts
+++ services/character-service/src/main.ts
@@ -1,10 +1,21 @@
 import express from 'express';
+import { jwtMiddleware } from './middlewares/jwt.middleware';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
   res.json({service: 'character-service', status: 'ok' });
+});
+
+app.get("/me", jwtMiddleware, (req, res) => {
+  res.json({
+    userId: req.user?.id,
+    role: req.user?.role
+  });
 });
 
 const PORT = process.env.PORT || 3002;
